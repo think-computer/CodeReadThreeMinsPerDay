cf6a3637c4946df8bc9bf83020a2c877308b0e28
JNU-YechanKim <48209701+JNU-YechanKim@users.noreply.github.com>
2019-06-12 00:53:03 +0900

    MYBATIS TEST

--- src/main/java/kr/ac/jejunu/educom/yechankim/YechankimApplication.java
+++ src/main/java/kr/ac/jejunu/educom/yechankim/YechankimApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class YechankimApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(YechankimApplication.class, args);
     }
-
 }
--- src/test/java/kr/ac/jejunu/educom/yechankim/YechankimApplicationTests.java
+++ src/test/java/kr/ac/jejunu/educom/yechankim/YechankimApplicationTests.java
@@ -2,15 +2,26 @@ package kr.ac.jejunu.educom.yechankim;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class YechankimApplicationTests {
 
+    @Autowired
+    private SqlSessionTemplate sqlSession;
+
+//    @Test
+//    public void contextLoads() {
+//    }
+
     @Test
-    public void contextLoads() {
+    public void testSqlSession() throws Exception {
+        System.out.println(sqlSession.toString() + "김예찬 test");
     }
 
 }
