7212778b01ed9dcb3557633bf19f314611fccc93
JNU-YechanKim <48209701+JNU-YechanKim@users.noreply.github.com>
2019-06-12 16:17:07 +0900

    게시판 추가/삭제/수정 기능 +
    지금 개발이 잘 되고 있길 바라며...

--- src/main/java/kr/ac/jejunu/educom/yechankim/controller/BoardController.java
+++ src/main/java/kr/ac/jejunu/educom/yechankim/controller/BoardController.java
@@ -5,6 +5,7 @@ import kr.ac.jejunu.educom.yechankim.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    private static String boardListURI = "redirect:/board/boardList";
 
     @RequestMapping("/board/boardList")
     public ModelAndView openBoardList() throws Exception {
@@ -24,9 +26,34 @@ public class BoardController {
         return mv;
     }
 
-    /*
-    @RequestMapping("/board/boardList")
-    public void openBoardList() throws Exception {
+    @RequestMapping("/board/boardWrite")
+    public void openBoardWrite() throws Exception { }
 
-    }*/
+    @RequestMapping("/board/insertBoard") // 실제 DB 삽입 후 boardList로 리다이렉트.
+    public String insertBoard(BoardDto board) throws Exception {
+        boardService.insertBoard(board);
+        return boardListURI;
+    }
+
+    @RequestMapping("/board/boardDetail")
+    public ModelAndView openBoardDetail(@RequestParam int boardIdx) throws Exception {
+        ModelAndView mv = new ModelAndView("/board/boardDetail");
+
+        BoardDto board = boardService.selectBoardDetail(boardIdx);
+        mv.addObject("board", board);
+
+        return mv;
+    }
+
+    @RequestMapping("/board/updateBoard")
+    public String updateBoard(BoardDto board) throws Exception {
+        boardService.updateBoard(board);
+        return boardListURI;
+    }
+
+    @RequestMapping("/board/deleteBoard")
+    public String deleteBoard(int boardIdx) throws Exception {
+        boardService.deleteBoard(boardIdx);
+        return boardListURI;
+    }
 }
--- src/main/java/kr/ac/jejunu/educom/yechankim/service/BoardService.java
+++ src/main/java/kr/ac/jejunu/educom/yechankim/service/BoardService.java
@@ -6,4 +6,12 @@ import java.util.List;
 
 public interface BoardService {
     List<BoardDto> selectBoardList() throws Exception;
+
+    void insertBoard(BoardDto board) throws Exception;
+
+    BoardDto selectBoardDetail(int boardIdx) throws Exception;
+
+    void updateBoard(BoardDto board) throws Exception;
+
+    void deleteBoard(int boardIdx) throws Exception;
 }
